011e10539fbc1f909f524f43df2526a84af00e29
TechWave Dynamics <techwavedy@gmail.com>
2026-08-17 20:02:08 +0530

    initial project setup

--- DataFetchingServerActions/datafetching/app/page.tsx
+++ DataFetchingServerActions/datafetching/app/page.tsx
@@ -1,69 +1,36 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import Link from "next/link";
+
+const lessions = [
+  {
+    id: "01",
+    title: "Lession 1",
+    href: "/lession-one",
+    description: "",
+  },
+  {
+    id: "02",
+    title: "Lession 2",
+    href: "/lession-two",
+    description: "",
+  },
+];
+
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
-          </h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <section className="page hero">
+      <div className="lesson-grid">
+        {lessions.map((lession) => (
+          <Link href={lession.href} key={lession.id} className="lesson-card">
+            <span>{lession.id}</span>
+            <h2>{lession.title}</h2>
+            <p>{lession.description}</p>
+            <strong>Open Example</strong>
+          </Link>
+        ))}
+      </div>
+    </section>
   );
 }
